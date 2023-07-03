5b5dc7daf56e4aa5743f744c8d57f7514d526552
Ukaypromise <promiseukaimo@gmail.com>
2023-07-03 22:08:01 +0100

    Modifications for turbo stream security

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -15,7 +15,7 @@ class QuotesTest < ApplicationSystemTestCase
 
   test "Creating a new quote" do
     visit quotes_path
-    assert_selector "h1", text: "Quotes"
+    assert_selector "h1", text: "Logger"
 
     click_on "New quote"
     fill_in "Name", with: "Capybara quote"
