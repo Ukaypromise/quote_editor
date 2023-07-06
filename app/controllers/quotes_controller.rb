bb1d06ae4052def7a0f27b09c527f62bd4c4bb64
Ukaypromise <promiseukaimo@gmail.com>
2023-07-06 11:24:16 +0100

    Add CI_CD workflow

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
     before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = current_company.quotes.ordered
+    @quotes = current_company.quotes.ordered 
   end
 
   def show
