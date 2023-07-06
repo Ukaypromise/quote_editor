fb2b64805d756a834f8986594320874773d55828
Ukaypromise <promiseukaimo@gmail.com>
2023-07-06 12:41:07 +0100

    Test passes

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -15,7 +15,7 @@ class QuotesTest < ApplicationSystemTestCase
 
   test "Creating a new quote" do
     visit quotes_path
-    assert_selector "h1", text: "Logger"
+    assert_selector "h1", text: "Quotes"
 
     click_on "New quote"
     fill_in "Name", with: "Capybara quote"
